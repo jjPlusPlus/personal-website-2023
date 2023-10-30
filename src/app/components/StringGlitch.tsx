301ef10507b2aeff96bb62f82333faac0c845501
JJ Medina <justinjcode@gmail.com>
2023-10-30 13:17:43 -0700

    Bring in a cool component to show off my name

---
+++ src/app/components/StringGlitch.tsx
@@ -0,0 +1,61 @@
+'use client'
+
+import React, { useState, useEffect } from 'react'
+
+// need to make props typescript
+interface StringGlitchProps {
+  text: string
+  interval: number
+}
+
+const StringGlitch = (props:StringGlitchProps) => {
+  const { text, interval } = props
+  const [formattedString, setNewString] = useState(["#", "%", "@", "#", "$", "!", "*", "$"])
+  const [count, setCount] = useState(0)
+  const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*"
+
+  let seed = []
+  for (var n = 0; n < text.length; n++) {
+    const characterIndex = Math.floor(Math.random() * characters.length)
+    const character = characters[characterIndex]
+    seed.push(character)
+  }
+
+  const [formatted, setFormatted] = useState(seed)
+
+  // Change each letter n times where n is the position of the letter in the string
+  useEffect(() => {
+    let changeInterval:any
+
+    changeInterval = setInterval(() => {
+      // for each letter
+      for (var i = 0; i < text.length; i++) {
+        // if the index is less than count, do nothing
+        if (i < count) {
+          // do nothing
+        } else if (i === count) {
+          // if this is the current letter, set it to the correct letter
+          formatted[i] = text[i]
+          setFormatted(formatted)
+        } else if ( count <= text.length ) {
+          // select a random character
+          const characterIndex = Math.floor(Math.random() * characters.length)
+          const character = characters[characterIndex]
+          // swap out that character at index
+          formatted[i] = character
+          setFormatted(formatted)
+        }
+      }
+      count >= text.length + 8 ? setCount(0) : setCount(count + 1)
+      setNewString(formatted)
+    }, 200)
+
+    return () => clearInterval(changeInterval)
+  }, [formattedString, text, characters, interval, count, formatted])
+
+  return (
+    <span>{formattedString}</span>
+  )
+}
+
+export default StringGlitch
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
 import Image from 'next/image'
+import StringGlitch from 'components/StringGlitch'
 
 export default function Home() {
   return (
     <main className="">
       <div>
-        <h1 className="font-mono">
-          Justin Medina
+        <h1 className="text-5xl">
+          <StringGlitch text="Justin Medina" interval={200} />
         </h1>
       </div>
     </main>
